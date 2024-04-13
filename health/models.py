d347ddecf9bc4091dd22b9cd1f82d58a1f4b5700
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-04-13 10:25:00 +0530

    new

--- health/models.py
+++ health/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+from .choices import DOCTOR_STATUS
+
+class Patient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    contact = models.ChartField(max_length=100, null=True)
+    address = models.CharField(max_length=100,null=True)
+    dob =  models.DataField(null=True)
+    image = models.FileField(null=True)
+
+    def __str__(self):
+        return self.user.username  
 # Create your models here.

de1f9bc18fce327116bc1cc9e735d41c75865610
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-04-14 12:59:19 +0530

    add-the-models

--- health/views.py
+++ health/views.py
@@ -161,7 +161,21 @@ def Change_Password(request):
                 d = {'error':error,'terror':terror,'data':sign}
                 return render (request,'change_password.html',d)
             
-            
+def preprocess_input(df,scaler):
+    csv_file = Admin_Helath_CSV.objects.get(id=1)
+    df = pd.read_csv(csv_file.csv_file)
+    X = df[['age','sex','cp',  'trestbps',  'chol',  'fbs',  'restecg',  'thalach',  'exang',  'oldpeak',  'slope',  'ca',  'thal']]
+    y = df['target']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=0)
+    nn_model = GradientBoostingClassifier(n_estimators=100,learning_rate=1.0,max_depth=1, random_state=0)
+    nn_model.fit(X_train, y_train)
+    pred = nn_model.predict([list_data])
+    print("Neural Network Accuracy: {:.2f}%".format(nn_model.score(X_test, y_test) * 100))
+    print("Prdicted Value is : ", format(pred))
+    dataframe = str(df.head())
+    return (nn_model.score(X_test, y_test) * 100),(pred)
+
+
 
         
 
